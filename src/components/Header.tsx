ec60f5e0ac8ae0f8fc9e3cc2be51ec6388c94b80
Alexy Martinet <amlds.dev@gmail.com>
2022-12-20 11:21:42 +0100

    debug btn contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,14 @@ import React from 'react';
 export default function Header() {
   return (
     <header>
-      <div>
+      <div className='headerText'>
         <h3>Alexy Martinet</h3>
-        <p>@amlds</p>
+        <p>@AMLDS</p>
         <p>Developper / Designer</p>
       </div>
-
-      {/* <div className="contact">
-      <svg width="400px" height="74" viewBox="0 0 399 74" fill="none">
-        <path d="M324.5 2.5H74.5L5.5 71.5H393.5L324.5 2.5Z" stroke="#FAF7F2" stroke-width="4"/>
-      </svg>
-      <span className='contact__text'>
-        <p>CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ </p>
-      </span>
-      </div> */}
-
       <div className="contact">
         <span className='contact__text'>
-          <p>CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ </p>
+          <p>CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ CONTACT ○ </p>
         </span>
       </div>
     </header>

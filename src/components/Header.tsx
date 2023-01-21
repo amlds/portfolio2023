4aad8fa0d8b434cec97683b9d8d4acc122fb42fa
Alexy Martinet <amlds.dev@gmail.com>
2023-01-21 16:09:08 +0100

    add animation & css for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import HoverLink from "./HoverLink";
+import Logo from "./svg/Logo";
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
 
 const Header: React.FC = () => {
+  const [lastScrollDirection, setLastScrollDirection] = useState<'up' | 'down'>('up');
+  const [isHeaderHidden, setIsHeaderHidden] = useState(false);
+
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      if (event.deltaY > 0) {
+        setLastScrollDirection('down');
+      } else {
+        setLastScrollDirection('up');
+      }
+    };
+
+    window.addEventListener('wheel', handleWheel);
+    return () => window.removeEventListener('wheel', handleWheel);
+  }, []);
+
+  useEffect(() => {
+    if (lastScrollDirection === 'down') {
+      setIsHeaderHidden(true);
+    } else {
+      setIsHeaderHidden(false);
+    }
+  }, [lastScrollDirection]);
+
   return (
-    <header>
-      <Link to="/">
-        <HoverLink infos={
-          {
-            name: "Projets",
-            font: ["1.5rem", "Satoshi"]
-          }
-        }/>
-      </Link>
-      <Link to="/about">
-        <HoverLink infos={
-          {
-            name: "À propos",
-            font: ["1.5rem", "Satoshi"]
-          }
-        }/>
-      </Link>
+    <header className={`header ${isHeaderHidden ? "header--hidden" : ""}`}>
+      <div>
+        <Logo />
+      </div>
+      <nav>
+        <Link to="/" onClick={scrollToTop}>
+          <HoverLink infos={
+            {
+              name: "Projets",
+              font: ["1.5rem", "Satoshi"]
+            }
+          }/>
+        </Link>
+        <Link to="/about" onClick={scrollToTop}>
+          <HoverLink infos={
+            {
+              name: "À propos",
+              font: ["1.5rem", "Satoshi"]
+            }
+          }/>
+        </Link>
+      </nav>
     </header>
   );
 }

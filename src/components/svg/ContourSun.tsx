e27afa324c3eb85327c5386fe2062d065f356db6
Alexy Martinet <amlds.dev@gmail.com>
2023-01-29 17:10:45 +0100

    button theme & header

---
+++ src/components/svg/ContourSun.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContourSun: React.FC = () => {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15.8993 1V4.33333M31 16.1515H27.7785M26.4698 26.7576L24.255 24.5354M15.8993 31V28.0707M5.32886 26.7576L7.54362 24.5354M1 16.1515H4.02013M5.32886 5.54545L7.54362 7.76768M26.4698 5.54545L24.255 7.76768" stroke="var(--text-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id='contourSun'/>
+    </svg>
+  );
+};
+
+export default ContourSun;
